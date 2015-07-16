4d22b834434c10fbcaefa241d23ac418544b02bf
Léo Lam <leo@innovatetechnologi.es>
2015-07-16 21:29:10 +0800

    Don't throw an error if there is no resolveFailed

--- src/route-segment.js
+++ src/route-segment.js
@@ -455,9 +455,6 @@ mod.provider( '$routeSegment',
                             var newResolve = {error: function() { return $q.when(error); }};
                             return resolve(index, name, angular.extend({resolve: newResolve}, params.resolveFailed));
                         }
-                        else
-                            throw new Error('Resolving failed with a reason `'+error+'`, but no `resolveFailed` ' +
-                                            'provided for segment `'+name+'`');
                     })
         }
 
