8d572abaadff449552e92f0d09bc48096de2d9d4
Léo Lam <leo@innovatetechnologi.es>
2016-04-28 11:46:43 +0200

    Drop build files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,7 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        
         pkg: grunt.file.readJSON('package.json'),
-                
-        uglify: {
-            options: {
-                banner: "/**\n * angular-route-segment <%=pkg.version%>\n * https://angular-route-segment.com\n * @author Artem Chivchalov\n * @license MIT License http://opensource.org/licenses/MIT\n */\n"
-            },
-            prod: {
-                files: {
-                    'build/angular-route-segment.min.js': ['build/angular-route-segment.js']
-                }
-            }
-        },
-        
-        concat: {
-            options: {
-                separator: ';',
-                banner: "/**\n * angular-route-segment <%=pkg.version%>\n * https://angular-route-segment.com\n * @author Artem Chivchalov\n * @license MIT License http://opensource.org/licenses/MIT\n */\n"
-            },
-            prod: {
-                src: ['src/**/*.js'],
-                dest: 'build/angular-route-segment.js'
-            }
-        },        
-        
         karma: {
             angular12: {
                 options: {
@@ -52,22 +28,7 @@ module.exports = function(grunt) {
                 }
             }
         },
-
-        "git-describe": {
-            run: {
-                options: {
-                    prop: 'gitdescribe'
-                }
-            }
-        }
-        
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-git-describe');
-        
-    grunt.registerTask('default', ['concat:prod']);
-    grunt.registerTask('prod', ['git-describe:run', 'concat:prod', 'uglify']);
 };
